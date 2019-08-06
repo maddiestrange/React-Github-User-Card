49b458dbe655963a84534d847c983bd09fd7c6a8
maddiestrange <maddie.strange@gmail.com>
2019-08-06 16:45:56 -0500

    starting styling

--- react-github-user-card/src/components/FollowerCards.js
+++ react-github-user-card/src/components/FollowerCards.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import 'semantic-ui-react';
 
 const FollowerCards = props => {
   // const sortedList = props.groceries.sort((a, b) => a.purchased - b.purchased);
   return (
-    <div>
+    <div class="ui link cards">
     {props.data.map(follower => {
       return ( 
-      <>
-      <h2>{follower.login}</h2>
-      <img width='200' src={follower.avatar_url}/></>
+      <div class="card">
+    <div class="image">
+      <img src={follower.avatar_url}/>
+    </div>
+      <div class="content"> 
+      <div class="header">{follower.login}</div>
+     </div>
+      </div>
       )})}
       </div>
   );
